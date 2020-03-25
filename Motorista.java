6bee40b64e84150449cbec344b86d4fcfbf9862e
julianebea <julianebeatrycce@gmail.com>
2020-03-24 21:49:48 -0300

    Mensagem de commit:

---
+++ Motorista.java
@@ -0,0 +1,18 @@
+public class Motorista{
+
+    public String getCPF(){
+        return cpf;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public Veiculo getVeiculo(){
+        return veiculo;
+    }
+
+    public FormaPagto[] getFormaPgto(){
+        return formaPgto;
+    }
+}
