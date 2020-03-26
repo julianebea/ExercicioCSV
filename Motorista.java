14e42ef777ae5c4105f207e70d6c547c2e176122
julianebea <julianebeatrycce@gmail.com>
2020-03-25 22:13:29 -0300

    ajuste das classes Veiculo e Motorista

--- Motorista.java
+++ Motorista.java
@@ -1,5 +1,17 @@
 public class Motorista{
 
+    private String cpf;
+    private String nome;
+    private Veiculo veiculo;
+    private FormaPagto[] formaPgto; 
+
+    public Motorista(String cpf, String nome, Veiculo veiculo, FormaPagto[] formaPgto){
+        this.cpf = cpf; 
+        this.nome = nome;
+        this.veiculo = veiculo;
+        this.formaPgto = formaPgto;
+    }
+
     public String getCPF(){
         return cpf;
     }
--- Veiculo.java
+++ Veiculo.java
@@ -1,5 +1,17 @@
 public class Veiculo{
 
+    private String placa;
+    private String marca;
+    private String cor;
+    private categoriaVeiculo categoria;
+
+    public Veiculo(String placa, String marca, String cor, categoriaVeiculo categoria){
+        this.placa = placa;
+        this.marca = marca;
+        this.cor = cor; 
+        this.categoria = categoria;
+    }
+
     public String getPlaca(){
         return placa;
     }
